d5376c5aad3936f1fa5cf09ccd68995a8e063520
vapppu <veera.hiltunen@gmail.com>
2023-04-21 11:28:33 +0300

    Haaste 7 valmis.

---
+++ 07-kivi-paperi-ja-sakset/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Haaste7
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vaihtoehdot = { "kivi", "paperi", "sakset" };
+            Dictionary<string, string> voittajat = new Dictionary<string, string>() {
+                {"kivi", "paperi"}, {"paperi", "sakset"}, {"sakset", "kivi"}
+            };
+            while (true)
+
+            {
+                string valinta = kayttajanValinta(vaihtoehdot);
+                Console.WriteLine($"Valitsit: {valinta}.");
+
+                Random rnd = new Random();
+                string tietokoneenValinta = vaihtoehdot[rnd.Next(0, 3)];
+                Console.WriteLine($"Tietokone valitsi: {tietokoneenValinta}.");
+
+                if (voittajat[valinta] == tietokoneenValinta)
+                {
+                    Console.WriteLine("Tietokone voitti!");
+                    break;
+                }
+                else if (voittajat[tietokoneenValinta] == valinta)
+                {
+                    Console.WriteLine("Voitit!");
+                    break;
+                }
+                else
+                    Console.WriteLine("Tasapeli! Otertaan uusintakierros.");
+            }
+        }
+
+        static string kayttajanValinta(string[] vaihtoehdot)
+        {
+            while (true)
+            {
+                Console.WriteLine("1 = Kivi, 2 = Paperi, 3 = Sakset?");
+                try
+                {
+                    return (vaihtoehdot[Convert.ToInt32(Console.ReadLine()) - 1]);
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
