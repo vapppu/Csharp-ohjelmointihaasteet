fa8da878dff9f719d96e49230707d34c83bfa26b
vapppu <veera.hiltunen@gmail.com>
2023-04-25 13:59:07 +0300

    gitignore added

---
+++ 13-numerot-merkkijonoksi/Program.cs
@@ -0,0 +1,42 @@
+﻿#nullable disable
+
+namespace Haaste13
+{
+    using System;
+
+    public class Program
+    {
+        string[] ykkoset = new string[] { "nolla", "yksi", "kaksi", "kolme", "neljä", "viisi", "kuusi", "seitsemän", "kahdeksan", "yhdeksän" };
+
+        string[] kymmenet = new string[] { "kymmenen", "kymmentä" };
+
+        string[] sadat = new string[] { "sata", "sataa" };
+
+        string[] tuhannet = new string[] { "tuhat", "tuhatta" };
+
+        static void Main(string[] args)
+        {
+            int numero = 120;
+            StringNumero numeroMerkkeina = new StringNumero(numero);
+            Console.WriteLine(numeroMerkkeina.merkkijonona());
+        }
+
+        public class StringNumero
+        {
+            int numero;
+            string numeroString;
+            // string[] ykkoset, kymmenet, sadat, tuhannet;
+            public StringNumero(int numero)
+            {
+                this.numero = numero;
+                this.numeroString = numero.ToString();
+            }
+            public string merkkijonona()
+            {
+                return ("1");
+
+            }
+
+        }
+    }
+}
